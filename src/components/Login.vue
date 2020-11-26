6aa06a218da1f9ae580e2251b449ab2bcdc655b1
dailingfeng <dtjqh5252@hotmail.com>
2020-11-26 16:56:24 +0800

    登录路由

---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">登陆界面</div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b5b6b;
+  height: 100%;
+}
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+</style>
