c8b64d186333b79e45c92b963ca28d7c9a5279ea
dailingfeng <dtjqh5252@hotmail.com>
2020-11-28 16:25:00 +0800

    侧边栏完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -21,22 +21,30 @@
           active-text-color="#ffd04b"
         >
           <!-- 一级菜单 -->
-          <el-submenu index="1">
+          <el-submenu
+            :index="item.id+''"
+            v-for="item in menuList"
+            :key="item.id"
+          >
             <!-- 一级菜单模板 -->
             <template slot="title">
               <!-- 图标 -->
-              <i class="el-icon-location"></i>
+              <i :class="iconsObj[item.id]"></i>
               <!-- 文本 -->
-              <span>导航一</span>
+              <span>{{item.authName}}</span>
             </template>
             <!-- 二级子菜单 -->
-            <el-menu-item index="1-4-1">
+            <el-menu-item
+              :index="subItem.id+''"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
               <!-- 二级菜单模板 -->
               <template slot="title">
                 <!-- 图标 -->
-                <i class="el-icon-location"></i>
+                <i :class="iconsObj[item.id]"></i>
                 <!-- 文本 -->
-                <span>子菜单一</span>
+                <span>{{subItem.authName}}</span>
               </template>
             </el-menu-item>
           </el-submenu>
@@ -49,10 +57,34 @@
 
 <script>
 export default {
+  data() {
+    return {
+      // 左侧菜单数据
+      menuList: null,
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      }
+    }
+  },
+  created() {
+    // 在 created 阶段请求左侧菜单数据
+    this.getMenuList()
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList() {
+      // 发送请求获取左侧菜单数据
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+      // console.log(res)
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -9,7 +9,11 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
+axios.interceptors.request.use((config) => {
+  // 为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 new Vue({
   router,
   render: (h) => h(App)
