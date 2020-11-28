07bdf7c6081e4888e3525470b106965b021f3336
dailingfeng <dtjqh5252@hotmail.com>
2020-11-28 14:27:00 +0800

    登录完成

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+// 设置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/home.vue'
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
 })
-
+router.beforeEach((to, from, next) => {
+  // to and from are both route objects. must call `next`.
+  if (to.path === '/login') return next()
+  // 获取 token
+  const tokenStr = window.sessionStorage.getItem('token')
+  // token 不存在，跳转登录界面
+  if (!tokenStr) return next('/login')
+  // token 存在，放行
+  next()
+})
 export default router
