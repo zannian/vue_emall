bc6db6655b05ad7144f66d54611336cdb0f69e91
dailingfeng <dtjqh5252@hotmail.com>
2020-12-01 09:47:09 +0800

    权限渲染

--- src/components/rights/roles.vue
+++ src/components/rights/roles.vue
@@ -1,43 +1,100 @@
 <template>
-  <el-table
-    row-key="id"
-    :data="roleList"
-    border
-  >
-    <!-- 添加展开列 -->
-    <el-table-column type="expand"></el-table-column>
-    <el-table-column type="index"></el-table-column>
-    <el-table-column
-      label="角色名称"
-      prop="roleName"
-    ></el-table-column>
-    <el-table-column
-      label="角色描述"
-      prop="roleDesc"
-    ></el-table-column>
-    <el-table-column
-      label="操作"
-      width="300px"
-    >
-      <template>
-        <el-button
-          size="mini"
-          type="primary"
-          icon="el-icon-edit"
-        >编辑</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          icon="el-icon-delete"
-        >删除</el-button>
-        <el-button
-          size="mini"
-          type="warning"
-          icon="el-icon-setting"
-        >分配权限</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card>
+      <!-- 添加角色按钮区域 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+
+      <el-table
+        :data="roleList"
+        border
+      >
+        <!-- 添加展开列 -->
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row
+              :class="['bdbottom',i1===0?'bdtop':'']"
+              v-for="(item1,i1) in scope.row.children"
+              :key="item1.id"
+            >
+              <!-- 渲染一级权限 -->
+              <el-col :span="5">
+                <el-tag> {{item1.authName}} </el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!-- 渲染二，三级权限 -->
+              <el-col :span="19">
+                <!-- 通过for循环嵌套渲染二级权限  -->
+                <el-row
+                  :class="[i2===0?'':'bdtop' ]"
+                  v-for="(item2,i2) in item1.children"
+                  :key="item2.id"
+                >
+                  <el-col :span="6">
+                    <el-tag type="success">{{item2.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-tag
+                      type="warning"
+                      v-for="(item3) in item2.children"
+                      :key="item3.id"
+                    >
+                      {{item3.authName}}
+                    </el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
+        <el-table-column
+          type="index"
+          label="#"
+        ></el-table-column>
+        <el-table-column
+          label="角色名称"
+          prop="roleName"
+        ></el-table-column>
+        <el-table-column
+          label="角色描述"
+          prop="roleDesc"
+        ></el-table-column>
+        <el-table-column
+          label="操作"
+          width="300px"
+        >
+          <template>
+            <el-button
+              size="mini"
+              type="primary"
+              icon="el-icon-edit"
+            >编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              icon="el-icon-delete"
+            >删除</el-button>
+            <el-button
+              size="mini"
+              type="warning"
+              icon="el-icon-setting"
+            >分配权限</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 <script>
 export default {
@@ -57,7 +114,7 @@ export default {
       //     return this.$message.error('获取角色列表失败')
       // //如果返回状态正常，将请求的数据保存在data中
       // this.roleList = res.data
-      console.log(res.data)
+      // console.log(res.data)
       this.roleList = res.data
     }
   }
